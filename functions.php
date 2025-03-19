02c78f77b656b6dae247b6e177f1b57a92ad38fa
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2025-03-19 12:42:02 -0700

    attempted to load normalize, but failing horribly

--- functions.php
+++ functions.php
@@ -1,16 +1,29 @@
 <?php
+
+
+function paws_enqueues()
+{
+
+	// Load normalize.css
+	wp_enqueue_style(
+		'paw-normalize',
+		get_theme_file_uri('/assets/css/normalize.css'),
+		array(),
+		'8.0.1'
+	);
+
 	// Load style.css on the front-end
 	// Parameters: Unique handle, Source, Dependencies, Version number, Media
+	wp_enqueue_style(
+		'paw-style',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get('Version'),
+		'all'
+	);
+}
 
-
-    // Load normalize.css
-
-
-
-
-
-
-
+add_action('wp_enqueue_scripts', 'paws_enqueues');
 
 
 // Custom Post Types & Custom Taxonomies
