781da6d705f5150bf41953a3bd7f55a09293bbb7
HHan0330 <kkoma330@gmail.com>
2025-03-21 13:29:22 -0700

    added the company phone number

--- paws-blocks/build/company-address/index.asset.php
+++ paws-blocks/build/company-address/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '42bc1df3cfbd52a86061');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '9e77f2681370ca464eb0');
