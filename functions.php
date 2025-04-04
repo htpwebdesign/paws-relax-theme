d3ec3c9642b3e7a2bb1e81d5673a0307f7ce02c8
HHan0330 <kkoma330@gmail.com>
2025-04-04 14:15:30 -0700

    added the allow svg code in function.php and changed the path for svg file, not using font awesome cdn

--- functions.php
+++ functions.php
@@ -232,14 +232,6 @@ function custom_taxonomy_link($url, $term, $taxonomy)
     return $url;
 }
 
-// Get a Icon from font-awesome
-function load_font_awesome()
-{
-    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css');
-}
-add_action('wp_enqueue_scripts', 'load_font_awesome');
-
-
 // Custom Login
 function custom_login_logo_url()
 {
@@ -486,3 +478,37 @@ function render_admin_notices_widget()
     </p>
 <?php
 }
+
+// Allow SVG
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+
+    global $wp_version;
+    if ( $wp_version !== '4.7.1' ) {
+       return $data;
+    }
+  
+    $filetype = wp_check_filetype( $filename, $mimes );
+  
+    return [
+        'ext'             => $filetype['ext'],
+        'type'            => $filetype['type'],
+        'proper_filename' => $data['proper_filename']
+    ];
+  
+  }, 10, 4 );
+  
+  function cc_mime_types( $mimes ){
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+  }
+  add_filter( 'upload_mimes', 'cc_mime_types' );
+  
+  function fix_svg() {
+    echo '<style type="text/css">
+          .attachment-266x266, .thumbnail img {
+               width: 100% !important;
+               height: auto !important;
+          }
+          </style>';
+  }
+  add_action( 'admin_head', 'fix_svg' );
